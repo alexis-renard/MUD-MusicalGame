29ab70efbb600a44baec97fc1f2ebd7486f11429
Denys Duchier <denys.duchier@univ-orleans.fr>
2014-01-20 21:13:28 +0100

    more player, start of engine

---
+++ mud/engine.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2014 Denys Duchier, IUT d'Orléans
+#==============================================================================
+
+import tornado.ioloop
+
+import threading, queue
+from mud.handlers.websocket import WebSocketHandler
+
+class Engine(threading.Thread):
+
+    def __init__(self):
+        super().__init__(daemon=True)
+        self._queue = queue.Queue()
+        self.put = self._queue.put
+        self.again = True
+
+    def run(self):
+        while self.again:
+            task = self._queue.get()
+            self.perform(task)
+
+    def perform(self, task):
+        meth = "perform_%s" % task["type"]
+        meth = getattr(self, meth)
+        meth(task)
+
+    def perform_input(self, task):
+        user    = task["user"]
+        text    = task["text"]
+        request = task["request"] # keep the request so that it can be reused for template rendering
+        html    = request.render_string(
+            "chat.html", user=user, text=text).decode("utf-8")
+        def callback():
+            WebSocketHandler.output_to_all(html)
+        tornado.ioloop.current().add_callback(callback)
+
+ENGINE = Engine()
+ENGINE.start()
---
+++ mud/player.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2014 Denys Duchier, IUT d'Orléans
+#==============================================================================
+
+from mud.db.transcript import DATABASE as TRANSCRIPTS
+
+class Player:
+
+    PLAYERS = {}
+
+    def __new__(cls, username):
+        player = cls.PLAYERS.get(username, None)
+        if player is not None:
+            return player
+        return super(Player, cls).__new__()
+
+    def __init__(self, username):
+        if hasattr(self, "username"): # check if the player has already been initialized
+            return                    # in which case, nothing more needs to be done
+        self.username = username      # otherwise, add the appropriate attributes
+        self.transcript = TRANSCRIPTS.lookup(username)
