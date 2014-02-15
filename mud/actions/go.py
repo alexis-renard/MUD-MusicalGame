3338a0bce4db4eeda3174e05fa322e28412998cf
Denys Duchier <denys.duchier@univ-orleans.fr>
2014-02-15 20:00:42 +0100

    use static.yml to normalize direction in go action

--- mud/actions/go.py
+++ mud/actions/go.py
@@ -3,9 +3,18 @@
 #==============================================================================
 
 from .action import Action2
-from mud.events import EnterEvent
+from mud.events import EnterPortalEvent
 
 class GoAction(Action2):
-    EVENT = EnterEvent
+    EVENT = EnterPortalEvent
     RESOLVE_OBJECT = "resolve_for_go"
     ACTION = "go"
+    RESOLVE_MAP = {}
+
+    def resolve_object(self):
+        if not self.RESOLVE_MAP:
+            from mud.static import STATIC
+            self.RESOLVE_MAP.update(STATIC["directions"]["normalized"])
+            self.RESOLVE_MAP.update((v,k) for (k,v) in STATIC["directions"]["noun_the"].items())
+            self.RESOLVE_MAP.update((v,k) for (k,v) in STATIC["directions"]["noun_at_the"].items())
+        return self.RESOLVE_MAP.get(self.object, None)
