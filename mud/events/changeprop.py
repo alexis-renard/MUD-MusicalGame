855410d6cee2fc7c0913c1063d8c3eb9e2605cad
Denys Duchier <denys.duchier@univ-orleans.fr>
2014-02-19 14:27:07 +0100

    add changeprop event

---
+++ mud/events/changeprop.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2014 Denys Duchier, IUT d'Orléans
+#==============================================================================
+
+from .event import Event2
+
+class ChangePropEvent(Event2):
+
+    def get_event_templates(self):
+        return self.actor.container().get_event_templates()
+
+    def execute(self):
+        props = self.object
+        if isinstance(props, str):
+            props = [props]
+        self.change_props(props, self.context())
