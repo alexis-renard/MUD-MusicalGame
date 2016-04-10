f101410647d3281fa2006595e6bc64e36367e8d0
alexis_renard <renard.alexis@hotmail.fr>
2016-04-10 11:32:47 +0200

    ajout du dossier alex, du fichier requirements.txt

--- mud.py
+++ mud.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 import mud.game
 import mud.server
-mud.game.GAME = mud.game.Game("greg")
+# mud.game.GAME = mud.game.Game("iut")
+# mud.game.GAME = mud.game.Game("greg")
+mud.game.GAME = mud.game.Game("alex")
 mud.game.GAME.load()
 mud.game.GAME.start()
