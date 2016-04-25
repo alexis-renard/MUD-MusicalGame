8acd94d1952d1209f884ae023a05b9518c85c72a
gmolveau <gr3ggz@gmail.com>
2016-04-25 23:42:40 +0200

    tentative fix playwith

--- mud/events/play.py
+++ mud/events/play.py
@@ -1,47 +1,29 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2014 Denys Duchier, IUT d'Orléans
 #==============================================================================
-
+from .event import Event2
 from .event import Event3
 from mud.models.mixins.containing import Containing
 from mud.models                   import Player
 import mud.game
 
 class PlayWithEvent(Event3):
+
+    def __init__(self, actor, object, object2):
+        Event2.__init__(self, actor, object2)
+        self.object2 = object
+
     NAME = "playwith"
 
     def perform(self):
-        if not self.object.has_prop("playable"):
-            self.add_prop("object-not-playable")
-        if self.object.has_prop("playable"):
-            if "instrument" in self.get_props():
-                for prop in self.get_props():
-                    if "000" in prop:
-                        #récupérer l'objet qui correspond au lieu loc
-                        loc = self.resolve_object(prop)
-                        self.actor.move_to(loc[0])
-                        return(self.inform("playwith.actor"))
+        if self.object2.has_prop("playable"):
+            print("partition ok")
+            if self.object.has_prop("instrument"):
+                print("instrument ok")
+                return(self.inform("playwith.actor"))
         return(self.play_failed())
-
-    def resolve_object(location):
-        world = mud.game.GAME.world
-        loc = world.get(location)
-        if loc:
-            locs = [loc]
-        else:
-            locs = []
-            for k,v in world.items():
-                if isinstance(v, Containing) and \
-                   not isinstance(v, Player) and \
-                   k.find(self.object) != -1:
-                    locs.append(v)
-        return locs
 
     def play_failed(self): ##à implémenter plus en profondeur iflwim
         self.fail()
         self.inform("playwith.failed")
 
-
-#initial
-    def perform(self):
-        self.inform("playwith") # de l'html
