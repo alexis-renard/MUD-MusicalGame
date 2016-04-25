a6335d377e7956f5b2eab660025a81fea9a708db
alexis_renard <renard.alexis@hotmail.fr>
2016-04-25 21:49:52 +0200

    changement avec fin du fix playwith

--- mud/events/play.py
+++ mud/events/play.py
@@ -9,3 +9,18 @@ import mud.game
 
 class PlayWithEvent(Event3):
     NAME = "playwith"
+
+    def perform(self):
+        print("base de la base")
+        if self.object.has_prop("playable"):
+            print("partition ok")
+            if self.object2.has_prop("instrument"):
+                print("instrument ok")
+                # return(self.play_failed())
+                return(self.inform("playwith.actor"))
+        return(self.play_failed())
+
+
+    def play_failed(self): ##à implémenter plus en profondeur iflwim
+        self.fail()
+        self.inform("playwith.failed")
