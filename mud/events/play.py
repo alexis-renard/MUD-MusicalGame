e3cea09661c9d1af503ed15d37384b5824e57ba1
gmolveau <gr3ggz@gmail.com>
2016-04-25 22:54:10 +0200

    fix loc list in play

--- mud/events/play.py
+++ mud/events/play.py
@@ -16,10 +16,10 @@ class PlayWithEvent(Event3):
         if self.object.has_prop("playable"):
             if "instrument" in self.get_props():
                 for prop in self.get_props():
-                    if "id" in prop:
+                    if "000" in prop:
                         #récupérer l'objet qui correspond au lieu loc
                         loc = self.resolve_object(prop)
-                        self.actor.move_to(loc)
+                        self.actor.move_to(loc[0])
                         return(self.inform("playwith.actor"))
         return(self.play_failed())
 
