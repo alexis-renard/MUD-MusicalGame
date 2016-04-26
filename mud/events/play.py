b84f4c0a307e7f3cf09ce7d5bbafbf226767dd0a
alexis_renard <renard.alexis@hotmail.fr>
2016-04-26 21:56:47 +0200

    fix bugg teleport

--- mud/events/play.py
+++ mud/events/play.py
@@ -2,21 +2,21 @@
 # Copyright (C) 2014 Denys Duchier, IUT d'Orléans
 #==============================================================================
 
-from .event import Event3
+from .event import Event2 ,Event3
 from mud.models.mixins.containing import Containing
 from mud.models                   import Player
 import mud.game
 
 class PlayWithEvent(Event3):
-    def __init__(self, actor, object, object2):
-        Event2.__init__(self, actor, object2)
-        self.object2 = object
+    # def __init__(self, actor, object, object2):
+    #     Event2.__init__(self, actor, object2)
+    #     self.object2 = object
     NAME = "playwith"
 
     def perform(self):
         if self.object2.has_prop("playable"):
             print("partition ok")
-            if self.object2.has_prop("instrument"):
+            if self.object.has_prop("instrument"):
                 print("instrument ok")
                 return(self.inform("playwith.actor"))
         return(self.play_failed())
